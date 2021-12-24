0eefcc4e53e63969b4fa5707f3e4ba6cd2521c61
davidramos9112010 <81586209+davidramos9112010@users.noreply.github.com>
2021-12-24 17:38:39 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,42 @@
-noseX = 0;
-noseY = 0;
-leftWristX= 0;
-rightWristX= 0;
-difference =0;
-function setup(){
-    video = createCapture(VIDEO);
-    video.size(550,500);
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
-    canvas=createCanvas(550,550);
-    canvas.position(560,110);
-poseNet=ml5.poseNet(video,modelLoaded);
-poseNet.on('pose',gotPoses);
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
 
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
 
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
-function modelLoaded()
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
+}
+
+
+function gotPoses(results)
 {
-    console.log('posenet is star hurry');
+  if(results.length > 0)
+  {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
-function gotPoses(results) 
-{ 
-    
-    if(results.length > 0) 
-    {
-console.log(results);
-noseX=results[0].pose.nose.x;
-noseY=results[0].pose.nose.y;
-console.log("noseX = " + noseX +" noseY = " + noseY);
-rightWristX=results[0].pose.rightWrist.x;
-leftWristX=results[0].pose.leftWrist.x;
-difference=floor(leftWristX-rightWristX);
-console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
 
-}
-}
-function draw()
-{
-background('#b2db0d');
-fill('#F90093');
- stroke('#F90093'); 
- square(noseX, noseY, difference);
+function draw() {
+background('#6C91C2');
 
- document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
-
-
+  document.getElementById("square_size").innerHTML = "Font size of the text will be = " + difference +"px";
+textSize(difference);
+fill('#FFE787');
+text('', 50, 400);
 }
